edd8fc8d8b63c1ea1a1f4bf0ad2d95211bc20f78
Akash-Mishra <mishraakash18@gmail.com>
2016-08-29 00:04:07 +0530

    changes in MainActivity.java

--- app/src/main/java/com/example/batcomputer/to_do/MainActivity.java
+++ app/src/main/java/com/example/batcomputer/to_do/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.batcomputer.to_do;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //added code
+        lvItems = (ListView)findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, R.layout.activity_main, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
+
     }
+
+
 }
