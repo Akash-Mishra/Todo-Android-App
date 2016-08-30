adaaf422cf30584cda5c7361dbbffc41470efedb
Akash-Mishra <mishraakash18@gmail.com>
2016-08-30 23:57:55 +0530

    add a todo button created

--- app/src/main/java/com/example/batcomputer/to_do/MainActivity.java
+++ app/src/main/java/com/example/batcomputer/to_do/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.batcomputer.to_do;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
 private static final String TAG = "MainActivity";
     @Override
@@ -31,8 +34,21 @@ private static final String TAG = "MainActivity";
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_add_task:
-                Log.d(TAG, "Add a new task");
-                return true;
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Add a new Task")
+                .setMessage("What do you want to do next?")
+                .setView(taskEditText)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        String task = String.valueOf(taskEditText.getText());
+                                        Log.d(TAG, "Task to add: " + task);
+                                    }
+                                })
+                .setNegativeButton("Cancel", null)
+                .create();
+//                return true;
 
             default:
                 return super.onOptionsItemSelected(item);
